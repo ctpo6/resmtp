d9fbbab49ae7a5f35eeec1ac35b26c78300b656c
Yuri Epstein <y.epstein@rambler-co.ru>
2015-12-23 16:20:25 +0300

    test script can get number of mails to send

---
+++ testmail/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+do_send_mail()
+{
+	mailx -r "yuri.epstein@rambler.ru" -s "test" -S smtp="inmx1.mail.rambler.ru:25" 25volt@25volt.ru < ./mail1.txt
+}
+
+if [ $# != 1 ]; then
+    echo "Usage: test.sh <n_mails>"
+    exit 1
+fi
+
+N_MAILS=$1
+
+echo "Sending $N_MAILS mails"
+
+# start N jobs in parallel
+for i in `seq 1 $N_MAILS`; do
+	do_send_mail &
+done
+
+echo "Waiting for finishing sending emails"
+wait
+
