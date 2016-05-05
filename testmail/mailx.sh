13bd0f03aec4d874247b7a21f6d61beab1b3bc2d
Yuri Epstein y.epstein@rambler-co.ru <y.epstein@rambler-co.ru>
2016-05-05 18:17:16 +0300

    mailx.sh: starttls

--- testmail/mailx.sh
+++ testmail/mailx.sh
@@ -6,7 +6,7 @@
 
 do_send_mail()
 {
-	mailx -r "yuri.epstein@rambler.ru" -s "test" -S smtp="resmtp.mail.rambler.ru:25" 25volt@25volt.ru < ./mail.txt
+	mailx -r "yuri.epstein@rambler.ru" -s "test_starttls" -S smtp="resmtp.mail.rambler.ru:25" -S smtp-use-starttls -S ssl-verify=ignore 25volt@25volt.ru < ./mail1.txt
 }
 
 if [ $# != 1 ]; then
