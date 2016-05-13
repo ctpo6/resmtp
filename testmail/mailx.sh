554eb7faba1caa272fba571c3afc91960c78ccc7
Yuri Epstein y.epstein@rambler-co.ru <y.epstein@rambler-co.ru>
2016-05-13 11:04:06 +0300

    testmail: mail file param in mailx.sh

--- testmail/mailx.sh
+++ testmail/mailx.sh
@@ -6,15 +6,17 @@
 
 do_send_mail()
 {
-	mailx -r "yuri.epstein@rambler.ru" -s "test_starttls" -S smtp="resmtp.mail.rambler.ru:25" -S smtp-use-starttls -S ssl-verify=ignore 25volt@25volt.ru < ./mail1.txt
+#	mailx -r "yuri.epstein@rambler.ru" -s "test_starttls" -S smtp="resmtp.mail.rambler.ru:25" -S smtp-use-starttls -S ssl-verify=ignore 25volt@25volt.ru < $MAIL_FILE
+	mailx -r "yuri.epstein@rambler.ru" -s "test_starttls" -S smtp="resmtp.mail.rambler.ru:25" 25volt@25volt.ru < $MAIL_FILE
 }
 
-if [ $# != 1 ]; then
-	echo "Usage: mailx.sh <mails>"
+if [ $# != 2 ]; then
+	echo "Usage: mailx.sh <n> <file>"
 	exit 1
 fi
 
 MAILS=$1
+MAIL_FILE=$2
 
 echo "Sending $MAILS mails..."
 
